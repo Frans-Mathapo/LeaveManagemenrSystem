2b5920c3999eed4c019d3edf0fe9e65220ad38b8
Frans-Mathapo <87014514+Frans-Mathapo@users.noreply.github.com>
2022-12-03 20:01:07 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagemenrSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
